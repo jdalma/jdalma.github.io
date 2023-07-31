5c65e6e4bf63ec48f29e556d874e73edbac300d1
jeongdalma <jeongdalma@gmail.com>
2023-07-31 20:56:06 +0900

    toc 수정

--- src/components/toc.js
+++ src/components/toc.js
@@ -2,13 +2,13 @@ import React from "react"
 
 const TableOfContents = ({ content }) => {
   return (
-    <div
-      // 스타일링을 위해서 클래스이름 부여 한다.
-      className="table-of-content"
-      // dangerouslySetInnerHTML는 보안 관점에서 위험하지만
-      // innerHTML을 사용하겠다는 뜻이다.
-      dangerouslySetInnerHTML={{ __html: content }}
-    />
+    <div className="toc-container">
+      <div className="toc-wrapper">
+        <div className="toc-content">
+          <div className="toc" dangerouslySetInnerHTML={{ __html: content }} />
+        </div>
+      </div>
+    </div>    
   )
 }
 
