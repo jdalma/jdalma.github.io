513a9ddfd9eac1c2e5c2ef514d9fe7f0b73d4b0b
jeongdalma <jeongdalma@gmail.com>
2023-09-05 00:40:08 +0900

    수정

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,7 +51,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 630,
+              maxWidth: 900,
             },
           },
           {
